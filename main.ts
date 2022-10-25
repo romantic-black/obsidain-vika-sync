6baa5c4d805e6fe6c78d8655425d8fe9d1cdff06
矫情之花 <63280964+romantic-black@users.noreply.github.com>
2022-10-25 16:09:02 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,82 +1,51 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { MyVika } from "utils/vika";
+import { MyNote, MyObsidian } from "utils/obsidian";
 
 // Remember to rename these classes and interfaces!
+//https://vika.cn/workbench/dstqMqKoMfmqwKcyGd/viwWEJsRNKaej/rec34ozZXsmYQ
+// token: ""
 
-interface MyPluginSettings {
-	mySetting: string;
+interface VikaSyncSettings {
+	token: string;
+	datasheet: string;
+	view: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: VikaSyncSettings = {
+	token: "",
+	datasheet: "",
+	view: ""
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+	settings: VikaSyncSettings;
+	vika: MyVika;
+	ob: MyObsidian;
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		try{
+			await this.loadSettings();
+		}
+		catch (e) {
+			console.log(e);
+		}
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'vika-sync-create-record',
+			name: 'Create Record',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
+				this.ob.createRecordInThisPage().then(res=> {
+					res?.success?new Notice("Record created"):new Notice("Record created failed");
+		})}});
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			id: 'vika-sync-update-record',
+			name: 'Update Record',
+			callback: () => {
+				this.ob.updateRecordInThisPage().then(res=> {
+					res?.success?new Notice("Record updated"):new Notice("Record updated failed");
+		})}});
+		this.addSettingTab(new SettingTab(this.app, this));
 	}
+
 
 	onunload() {
 
@@ -84,6 +53,8 @@ export default class MyPlugin extends Plugin {
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.vika = new MyVika(this.settings.token, this.settings.datasheet, this.settings.view);
+		this.ob = new MyObsidian(this.app, this.vika);
 	}
 
 	async saveSettings() {
@@ -91,23 +62,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class SettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -120,17 +75,34 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Vika Sync Settings'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Token')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.token)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.token = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Datasheet')
+			.addText(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.datasheet)
+				.onChange(async (value) => {
+					this.plugin.settings.datasheet = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('View')
+			.addText(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.view)
+				.onChange(async (value) => {
+					this.plugin.settings.view = value;
 					await this.plugin.saveSettings();
 				}));
 	}
