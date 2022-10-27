155a07ffa68b0af5751f043b86bcb4045fdbcc7c
AccelSynchro <21121471@bjtu.edu.cn>
2022-10-27 23:50:33 +0800

    <fix>这次才是真修复

--- utils/obsidian.ts
+++ utils/obsidian.ts
@@ -313,7 +313,7 @@ class MyNote {
         let fm_dict = this.parseFrontMatterDict(this.frontmatter);
         fm_dict = Object.assign(fm_dict, this.getFrontMatterFromRecord(this.settings.recoverField, record));
         let fm_text = this.dumpsFrontMatter(fm_dict);
-        let full_content = fm_text + '\n' + this.content;
+        let full_content = fm_text + this.content;
         this.app.vault.modify(this.file, full_content);
 
         this.updateInfo();
